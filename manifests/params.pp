9c611f3c6930a71350a3971ac07b4599aa8f9508
kjhenner <kjhenner@gmail.com>
2015-11-30 10:00:17 -0800

    new rc

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,5 @@
 class bootstrap::params {
-  $pe_version    = '2015.3.0-rc3-538-g1f9024c'
+  $pe_version    = '2015.3.0-rc4-206-gf839260'
   $agent_version = '1.2.2'
 
   $source_path   = '/usr/src/installer/'
