1a89343b0c0e3f0d197661f0c8d231cbbb720038
kjhenner <kjhenner@gmail.com>
2016-02-10 15:58:33 -0800

    adjust roles

--- manifests/role/learning.pp
+++ manifests/role/learning.pp
@@ -2,11 +2,11 @@ class bootstrap::role::learning {
   include epel
   include localrepo
   include bootstrap
-  include lms::course_selector
-  include lms::lab_repo
-  include bootstrap::profile::quest_content
-  include bootstrap::profile::learning_ssh
-  include bootstrap::profile::set_defaults
-  include bootstrap::profile::learning_splash
-  include bootstrap::profile::replace_factor
+  #include lms::course_selector
+  #include lms::lab_repo
+  #include bootstrap::profile::quest_content
+  #include bootstrap::profile::learning_ssh
+  #include bootstrap::profile::set_defaults
+  #include bootstrap::profile::learning_splash
+  #include bootstrap::profile::replace_factor
 }
